b4978da2bd96757e79d2c374f5e70c53c54460e6
rvaddadi <vaddadir@gmail.com>
2016-04-29 23:13:42 -0400

    Fixed code to create table only if table does not exist

--- PasswordManager/src/com/password/manager/dao/impl/CreateTablesImpl.java
+++ PasswordManager/src/com/password/manager/dao/impl/CreateTablesImpl.java
@@ -1,6 +1,8 @@
 package com.password.manager.dao.impl;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import com.password.manager.dao.CreateTables;
@@ -45,7 +47,7 @@ public class CreateTablesImpl implements CreateTables {
 			 Statement stmt  = null;
 			 String sqlQuery = null;
 			 stmt = con.createStatement();				
-			 sqlQuery = "CREATE TABLE  PCATEGORY (CATEGORY TEXT PRIMARY KEY  NOT NULL);";	                    			 
+			 sqlQuery = "CREATE TABLE IF NOT EXISTS PCATEGORY (CATEGORY TEXT PRIMARY KEY  NOT NULL);";	                    			 
 			 stmt.executeUpdate(sqlQuery);
 			 con.commit();
 			 stmt.close();			 
@@ -70,7 +72,7 @@ public class CreateTablesImpl implements CreateTables {
 			 Statement stmt  = null;
 			 String sqlQuery = null;
 			 stmt = con.createStatement();				
-			 sqlQuery = "CREATE TABLE  PPASSWORDDETAILS (ACCOUNT TEXT PRIMARY KEY  NOT NULL,CATEGORY TEXT NOT NULL,USERNAME  TEXT NOT NULL,PASSWORD  CHAR(50));";	                    			 
+			 sqlQuery = "CREATE TABLE IF NOT EXISTS PPASSWORDDETAILS (ACCOUNT TEXT PRIMARY KEY  NOT NULL,CATEGORY TEXT NOT NULL,USERNAME  TEXT NOT NULL,PASSWORD  CHAR(50));";	                    			 
 			 stmt.executeUpdate(sqlQuery);
 			 con.commit();
 			 stmt.close();			 
@@ -88,8 +90,40 @@ public class CreateTablesImpl implements CreateTables {
 	}
 	@Override
 	public void CreateAllTables() {
-		createUserTable();
-		createCategoryTable();
-		passwordInformationTable();		
+		if (!hasTable("PUSERPROFILE"))
+		{
+			createUserTable();
+		}
+		if (!hasTable("PCATEGORY"))
+		{
+			createCategoryTable();
+		}
+		if (!hasTable("PPASSWORDDETAILS")){
+			passwordInformationTable();	
+		}
+	}
+	
+	private boolean hasTable(String tableName)
+	{
+		Connection connection = null; 
+		try{
+			connection = DBConnection.getDBConnection();  
+			DatabaseMetaData dbMetaData = connection.getMetaData();
+			ResultSet tables = dbMetaData.getTables(null, null, tableName, 
+			         new String[] {"TABLE"});			
+			int numberOfTables = 0;
+			while (tables.next())
+			{
+				numberOfTables++;
+			}
+			connection.close();
+			return numberOfTables > 0;	      
+		}
+		catch(SQLException ex)
+		{
+			
+		}
+		
+		return false;
 	}
 }
